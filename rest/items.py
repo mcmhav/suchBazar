ccab1eff3d29cedf6b1112f6c8173f8899a65401
Herman Schistad <herman.schistad@gmail.com>
2014-02-20 16:30:19 +0100

    Now using mongodb

--- rest/items.py
+++ rest/items.py
@@ -2,54 +2,41 @@ import requests
 import time
 import json
 import sys
+import pymongo
 
 # Settings
 base_url = "https://goodiez-staging.appspot.com/api/goodiez"
-total_collections = 87
 username = "goodiez"
 password = "goodiez"
-output_file = "items.json"
-i = 1
+pageSize = 1000
+increment = 500
+cursorKey = None
 
-# Open a file for writing
-f = open(output_file, "w")
+# Mongo.db
+client = pymongo.MongoClient()
+db = client.mydb
+col = db["items"]
+col.remove()
 
-# Load collections
-collections_url = "%s/%s?pageSize=%s" % (base_url, 'collection/v10', total_collections)
-res = requests.get(collections_url, auth=(username, password))
-json_data = res.json()
-
-# Find items
-items = []
-
-for collection in json_data["items"]:
+while pageSize < 20000:
   # Increase counter and give status to user
-  print "Starting on collection %s of %s" % (i, total_collections)
-  i += 1
+  print "Getting offer %s to %s" % (pageSize, pageSize + increment)
+  pageSize += increment
 
-  collection_id = collection["id"]
-
-  offers_url = "%s/%s?collectionId=%s" % (base_url, "offer/v10", collection_id)
-
-  # Default to a large pageSize, in order to get alle items.
-  total_size = 1000
-
-  # Construct new url with all items
-  offers_url_complete = "%s&pageSize=%s" % (offers_url, total_size)
+  offers_url = "%s/%s?pageSize=%s" % (base_url, "offer/v10", increment)
+  if cursorKey:
+    offers_url = "%s&cursorKey=%s" % (offers_url, cursorKey)
 
   # Request all offers in this collection
   time.sleep(3)
-  res = requests.get(offers_url_complete, auth=(username, password))
+  res = requests.get(offers_url, auth=(username, password))
   if res.status_code != 200:
     print "Recieved status: %s, skipping." % res.status_code
     continue
   jdata = res.json()
 
-  print "Got collection id %s, found %s items. Adding ..." % (collection_id, jdata.get("totalSize", -1))
+  # Save the cursor key
+  cursorKey = jdata["cursorKey"]
 
   for item in jdata["items"]:
-    items.append(item)
-
-  # Write to file
-  f.seek(0)
-  f.write(json.dumps(items))
+    col.insert(item)

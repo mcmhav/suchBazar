8a78a3aaeb8b951b1a0977c8e6df126567334b35
Herman Schistad <herman.schistad@gmail.com>
2014-04-05 18:40:46 +0200

    Normalize the ratings

--- generators/utils.py
+++ generators/utils.py
@@ -24,15 +24,35 @@ def create_usermatrix(filename):
 def translate_events(events):
   # A list of events.
   multipliers = {
-    'featured_product_clicked': 1,
-    'product_detail_clicked': 1,
-    'product_wanted': 5,
-    'product_purchase_intended': 10,
-    'product_purchased': 15
+    'featured_product_clicked': [10, 20, 30, 40, 50],
+    'product_detail_clicked': [10, 20, 30, 40, 50],
+    'product_wanted': [55, 65, 75],
+    'product_purchase_intended': [90],
+    'product_purchased': [100]
   }
+  count = defaultdict(int)
   rating = 0
   for event in events:
-    rating += multipliers.get(event, 0)
+    # The number of times we've seen this event for this user
+    freq = count[event]
+
+    # Get the multipliers for this event.
+    multi = multipliers.get(event, [0])
+
+    # Get the index we will use to get score. Max is length of number of scores.
+    idx = min(freq, len(multi)-1)
+
+    # Just get the score. And if it is higher than existing rating, replace it.
+    rating = max(rating, multi[idx])
+
+    # Increase the count for this event.
+    count[event] += 1
+
+  # Normalize to k
+  t = rating
+  k = 5.0
+  rating = ((k-1)/100.0) * rating + 1
+
   return rating
 
 def products_to_file(user_id, products, f):

48ea6fb0fe2948bce701dda73f0f1eb4a7da57ef
Herman Schistad <herman.schistad@gmail.com>
2014-05-20 09:36:27 +0200

    Make it possible to randomize in split

--- generators/split.sh
+++ generators/split.sh
@@ -1,9 +1,41 @@
-if [ $# -ne 1 ]; then
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-r (randomize)] -i inputfile.txt"; exit 1; }
+
+split() {
+  awk '
+    BEGIN { srand() }
+    {f = rand() <= 0.9 ? "'"$2"'.9.txt" : "'"${2}"'.1.txt"; print > f}
+  ' $1
+}
+
+RAND=0;
+INFILE=""
+
+while getopts "ri:" o; do
+  case "${o}" in
+    r)
+      RAND=1
+      ;;
+    i)
+      INFILE=${OPTARG}
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+if [ -z "$INFILE" ]; then
   echo "Need to specify file to split.";
   exit 2;
 fi
 
-awk '
-  BEGIN { srand() }
-  {f = rand() <= 0.9 ? "training.txt" : "validation.txt"; print > f}
-' $1
+if [ $RAND -eq 1 ]; then
+  TMPFILE=/tmp/test_tmp.txt;
+  perl -MList::Util -e 'print List::Util::shuffle <>' $INFILE > $TMPFILE
+  split $TMPFILE $INFILE;
+  rm $TMPFILE
+else
+  split $INFILE, $INFILE
+fi

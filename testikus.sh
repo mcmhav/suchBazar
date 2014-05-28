723ad85c2263efe6208d20e66aadc197703ee584
Herman Schistad <herman.schistad@gmail.com>
2014-05-28 11:57:55 +0200

    Clean up flags

--- testikus.sh
+++ testikus.sh
@@ -15,25 +15,22 @@ ROOT=$( cd "$( dirname "$0" )" && pwd );
 # Some parameters changable in the opts.
 INFILE="-i ../datasets/v3/sobazar_events_prod_cleaned_formatted.tab"
 CLEAN=""
-MYMEDIAITEM=""
-MYMEDIAIRANK=""
-MAHOUT=""
 SPLIT=0
 
 # Available Item Recommenders:
 # 'BPRMF' 'ItemAttributeKNN' 'ItemKNN' 'MostPopular' 'Random' 'UserAttributeKNN' 'UserKNN' 'WRMF' 'Zero' 'MultiCoreBPRMF' 'SoftMarginRankingMF' 'WeightedBPRMF' 'BPRLinear' 'MostPopularByAttributes' 'BPRSLIM' 'LeastSquareSLIM'
-ITEMRECOMMENDERS="MostPopular WRMF BPRMF"
+ITEMRECOMMENDERS=""
 
 # Available Rank Recommenders:
 # 'BiPolarSlopeOne' 'GlobalAverage' 'ItemAttributeKNN' 'ItemAverage' 'ItemKNN' 'MatrixFactorization' 'SlopeOne' 'UserAttributeKNN' 'UserAverage' 'UserItemBaseline' 'UserKNN' 'TimeAwareBaseline' 'TimeAwareBaselineWithFrequencies' 'CoClustering' 'Random' 'Constant' 'LatentFeatureLogLinearModel' 'BiasedMatrixFactorization' 'SVDPlusPlus' 'SigmoidSVDPlusPlus' 'SocialMF' 'SigmoidItemAsymmetricFactorModel' 'SigmoidUserAsymmetricFactorModel' 'SigmoidCombinedAsymmetricFactorModel' 'NaiveBayes' 'ExternalRatingPredictor' 'GSVDPlusPlus'
-RANKRECOMMENDERS="MatrixFactorization NaiveBayes"
+RANKRECOMMENDERS=""
 
 #Available Mahout recommenders
 # 'svd' ...
-MAHOUTRECOMMENDERS="svd"
+MAHOUTRECOMMENDERS=""
 QUIET=''
 
-while getopts "i:cp:sr:h:tmq" o; do
+while getopts "i:cp:sr:m:tmq" o; do
   case "${o}" in
     i)
       INFILE="-i ${OPTARG}"
@@ -42,22 +39,18 @@ while getopts "i:cp:sr:h:tmq" o; do
       CLEAN="-c"
       ;;
     p)
-      MYMEDIAITEM="-i"
       ITEMRECOMMENDERS="${OPTARG}"
       ;;
     r)
-      MYMEDIAIRANK="-r"
       RANKRECOMMENDERS="${OPTARG}"
       ;;
-    h)
-      MAHOUT="-h"
+    m)
       MAHOUTRECOMMENDERS="${OPTARG}"
       ;;
     t)
-      MYMEDIAITEM="-i"
+      # Test a variety of good default options.
       ITEMRECOMMENDERS="BPRMF ItemKNN MostPopular Random UserKNN WRMF"
-      MAHOUT="-h"
-      MAHOUTRECOMMENDERS="svd"
+      RANKRECOMMENDERS="MatrixFactorization NaiveBayes"
       ;;
     s)
       SPLIT=1
@@ -72,7 +65,6 @@ while getopts "i:cp:sr:h:tmq" o; do
 done
 
 OPTS="$INFILE $CLEAN"
-RECOMMENDERSYS="$MYMEDIAITEM $MYMEDIAIRANK $MAHOUT"
 
 # Generate ratings (blending and timestamps enabled by default)
 /bin/bash $ROOT/generators/generate_implicit.sh -b -t $OPTS;
@@ -86,41 +78,37 @@ fi
 # Todo: can we avoid having such a long string which, to be honest, no one will change in near future anyways?
 trainTestTuples="blend_itemtrain1.txt:blend_itemtest1.txt blend_itemtrain2.txt:blend_itemtest2.txt blend_itemtrain3.txt:blend_itemtest3.txt blend_systemtrain1.txt:blend_systemtest.txt blend_systemtrain2.txt:blend_systemtest.txt blend_systemtrain3.txt:blend_systemtest.txt blend_usertrain1.txt:blend_usertest1.txt blend_usertrain2.txt:blend_usertest2.txt blend_usertrain3.txt:blend_usertest3.txt"
 
-if [[ $MYMEDIAITEM ]]; then
+if [ $ITEMRECOMMENDERS != "" ]; then
   for ir in $ITEMRECOMMENDERS
   do
     # make predictions
     echo "------------------------------"
-    echo "Running $ROOT/generators/myMediaLitePredicter.sh -t '$trainTestTuples' $MYMEDIAITEM -p $ir";
-    /bin/bash $ROOT/generators/myMediaLitePredicter.sh -t "$trainTestTuples" $MYMEDIAITEM -p $ir $QUIET;
-
-    echo "------------------------------"
+    /bin/bash $ROOT/generators/myMediaLitePredicter.sh -t "$trainTestTuples" -i -p $ir $QUIET;
 
     # evaluate predicted values
-    echo "Running $ROOT/evaluation/evaluate.sh -t '$trainTestTuples' -r $MYMEDIAITEM -p $ir -m";
-    /bin/bash $ROOT/evaluation/evaluate.sh -t "$trainTestTuples" -r $MYMEDIAITEM -p $ir -m;
+    /bin/bash $ROOT/evaluation/evaluate.sh -t "$trainTestTuples" -r -i -p $ir -m;
   done
 fi
 
-if [[ $MYMEDIAIRANK ]]; then
+if [ $RANKRECOMMENDERS != "" ]; then
   for ir in $RANKRECOMMENDERS
   do
     # make predictions
-    /bin/bash $ROOT/generators/myMediaLitePredicter.sh -t "$trainTestTuples" $MYMEDIAIRANK -p $ir;
+    /bin/bash $ROOT/generators/myMediaLitePredicter.sh -t "$trainTestTuples" -r -p $ir;
 
     # evaluate predicted values
-    /bin/bash $ROOT/generators/evaluate.sh -t "$trainTestTuples" -r $MYMEDIAIRANK -p $ir;
+    /bin/bash $ROOT/generators/evaluate.sh -t "$trainTestTuples" -r -p $ir;
   done
 fi
 
-if [[ $MAHOUT ]]; then
+if [ $MAHOUTRECOMMENDERS != "" ]; then
   for ir in $MAHOUTRECOMMENDERS
   do
     # make predictions
-    /bin/bash $ROOT/generators/mahoutPredict.sh -t "$trainTestTuples" $MAHOUT -p $ir;
+    /bin/bash $ROOT/generators/mahoutPredict.sh -t "$trainTestTuples" -h -p $ir;
 
     # evaluate predicted values
-    /bin/bash $ROOT/evaluation/evaluate.sh -t "$trainTestTuples" -r $MAHOUT -p $ir;
+    /bin/bash $ROOT/evaluation/evaluate.sh -t "$trainTestTuples" -r -h -p $ir;
   done
 fi
 
